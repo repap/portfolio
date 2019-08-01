6eec1257024f8c05c3c75d56a7d9ed649494e6cf
Dennis Penning <penning.dennis@googlemail.com>
2019-08-01 06:10:00 +0200

    add simple express server; static route and test api

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const {PORT} = process.env || 5000
+
+app.static('/', path.join(__dirname, '/build') )
+
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, '/build/') + 'index.html'))
+
+app.get('/api/test', (req, res) => res.send('HELLO :D'))
+
+app.listen(PORT, err => 
+  err ? 
+    console.error(err) : 
+    console.log(`app is listening to port ${PORT}`))
